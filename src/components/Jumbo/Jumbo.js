cbe204cdbf52460e386f7747bc713097e081db66
chilimango <maldonadot55@yahoo.com>
2021-03-02 19:27:08 -0800

    moved images folder to sources

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -5,12 +5,13 @@ function Jumbo() {
     return (
         <div className="jumbotron">
             <div className="container-fluid">
+                {/* <img src={require("./images/html-5.svg")} /> */}
                 <img src="./images/html-5.svg" alt="logo of html" />
                 <img src="./images/java.svg" alt="logo of java" />
                 <img src="./images/javascript.svg" alt="logo of javascript" />
-                <img src="./images/nodejs.svg" alt="logo of node" />
+                <img src="./images/nodejs.svg" alt="logo of node.js" />
                 <img src="./images/python.svg" alt="logo of python" />
-                <img src="./images/react.svg" alt="logo of react" />
+                <img src="./images/react.svg" alt="logo of react.js" />
             </div>
         </div>
     )
